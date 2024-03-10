a25e4a33c03dd58242a4e031b2b2de0a253f34b4
cwj <595842570@qq.com>
2024-03-10 15:44:32 +0800

    feat: 打包分析

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true', //当环境变量ANALYZE为true时开启
+});
+
+const nextConfig = {
+  sassOptions: {
+    additionalData: '@import "@/styles/index.scss";',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.test.com',
+      },
+    ],
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
