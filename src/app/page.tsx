d5a02a90d0018f633e9a5ae9fa3d85217fb20f71
cwjbjy <595842570@qq.com>
2024-03-14 09:05:07 +0800

    feat: 修改首页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { getNews } from '@/apis/api';
-
 export default async function Home() {
-  const list = await getNews();
-  return (
-    <article>
-      {list.map((o: any) => (
-        <div key={o.id}>{o.name}</div>
-      ))}
-    </article>
-  );
+  return <article>首页</article>;
 }
